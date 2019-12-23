69cd0942aa83ce7de91387bfa305cd47f900d6c7
Arezouma S Ngobou <sarezouma@gmail.com>
2019-12-23 12:36:08 -0500

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import './sign-in.styles.scss';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
     constructor(props){
@@ -16,11 +16,21 @@ class SignIn extends React.Component {
         };
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email: '', password: '' })
-    }
+        const { email, password } = this.state;
+
+        try {
+            await auth.createUserWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' });
+
+        } catch (error) {
+            console.error(error);
+    
+        }
+
+    };
 
     handleChange = event => {
         const { value, name } = event.target;
@@ -40,16 +50,16 @@ class SignIn extends React.Component {
                         type="email" 
                         handleChange={this.handleChange} 
                         value={this.state.email}
-                        label='email' 
+                        label='Email' 
                         required 
                     />
                     
                     <FormInput 
                         name="password" 
                         type="password" 
-                        value={this.state.email} 
+                        value={this.state.password} 
                         handleChange={this.handleChange}
-                        label='password' 
+                        label='Password' 
                         required 
                     />
                     <div className='buttons'>
